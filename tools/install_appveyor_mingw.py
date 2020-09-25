ba888e4411e8d8c071934b568cf5335253e57d8f
Pablo Estrada <pabloem@apache.org>
2020-09-25 12:13:07 -0700

    fixup2

--- tools/install_appveyor_mingw.py
+++ tools/install_appveyor_mingw.py
@@ -150,8 +150,9 @@ else:
 
 # Testing, packaging.
 if is_python_build:
+    # Run Python lint checks
     run_command(pip + ' install pycodestyle pylint mypy')
-    run_command(pycodestyle + ' pykep/')
+    run_command('pycodestyle pykep/')
     # Run the Python tests.
     run_command(
         pinterp + r' -c "from pykep import test; test.run_test_suite();"')
